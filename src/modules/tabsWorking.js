3263935d2da9ff598db28af1dd06932bf379f5fc
lena <elena_pilyushenko@mail.ru>
2021-01-17 00:45:42 +0300

    fixed

--- src/modules/tabsWorking.js
+++ src/modules/tabsWorking.js
@@ -42,8 +42,33 @@ const tabsWorking = () => {
         }
     });
 
-    let counetSlide = 1;
     
+    const widthWindow = window.innerWidth;
+    let counetSlide = 1;
+
+    const resizeWin = (widthWindow) => {
+
+        if (widthWindow >= 1135) {
+            counetSlide = 5;
+        }
+        if (widthWindow <= 1134 && widthWindow >= 1025) {
+            counetSlide = 4;
+        }
+        if (widthWindow <= 1024 && widthWindow >= 768) {
+            counetSlide = 2;
+        }
+        if (widthWindow < 767) {
+            counetSlide = 1;
+        }
+    };
+
+    resizeWin(widthWindow);
+
+    window.addEventListener('resize', (event) => {
+        resizeWin(window.innerWidth);
+    });
+    // console.log('counetSlide: ', counetSlide);
+
     const showSlidesTabs =() => {
         const carouselTabs = new sliderCarousel({
         main: '#scheme nav',
@@ -68,22 +93,11 @@ const tabsWorking = () => {
         carouselTabs.init();
     };
 
-    const widthWindow = document.documentElement.clientWidth;
-
-    if (widthWindow >= 1140) {
-        counetSlide = 5;
-    } else if (widthWindow <= 1039 && widthWindow >= 1025) {
-        counetSlide = 4;
-    } else if (widthWindow <= 1024 && widthWindow >= 768) {
-        counetSlide = 2;       
-    } else if (widthWindow < 767) {
-        counetSlide = 1;  
-    }
-    // console.log('counetSlide: ', counetSlide);
-
     if (widthWindow < 1135) {
         showSlidesTabs();
     }
+
+    
 };
 
 export default tabsWorking;
